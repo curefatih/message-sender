d3051ed60276aa6a17b52c213f200f9a390d76e8
cüre <curefat@gmail.com>
2024-07-27 14:18:06 +0300

    add postgresql connection initializer

---
+++ db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+
+	"github.com/curefatih/message-sender/model"
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+)
+
+func InitPostgreSQLConnection(ctx context.Context, dsn string) *gorm.DB {
+	println("dsn is:", dsn)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&model.MessageTask{})
+	db.AutoMigrate(&model.TaskState{})
+
+	return db
+}
